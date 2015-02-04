983d72a718684d4b286c0b6d1d4cbfb49a95a6d7
Todd Roth <toddjroth@gmail.com>
2015-02-03 22:29:16 -0500

    clean up

--- src/Main.hs
+++ src/Main.hs
@@ -6,26 +6,21 @@ import System.IO
 import Pipes
 import Pipes.HTTP
 import Data.Either (rights)
-import qualified Pipes.ByteString as PB  -- from `pipes-bytestring`
 import qualified Pipes.Prelude as PP
-import Pipes.Csv (decode, HasHeader(..), FromRecord(..), (.:), (.!))
+import Pipes.Csv (decode, HasHeader(..)) 
 import Data.Time
 import Data.Char (toUpper)
-import Data.Either (rights)
-import Data.List (tails)
 import qualified Data.Vector as V
 import qualified Data.Csv as CSV
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
 import Types
-import GHC.IO.Handle (hClose)
 
-import Control.Foldl (Fold(Fold), purely)
-import qualified Control.Foldl as L
-import Data.Sequence (Seq, (|>))
-import qualified Data.Sequence as S
 
-quoteDataFromProvider :: QuoteProvider  -- todo validate filepath ? directoryExists
+-----------------------------------------------------------------------------------
+-- Retrieves historical price data from server; currently only supports Yahoo data
+
+quoteDataFromProvider :: QuoteProvider 
                       -> Ticker
                       -- ^ The ticker symbol (eg Ticker "AAPL")
                       -> Integer
@@ -45,7 +40,9 @@ quoteDataFromProvider qp t nd p = do
       where
         quoteProducer resp = decode HasHeader (responseBody resp) -- :: Producer (Either String Quote) IO ()
 
-
+-----------------------------------------------------------------------------------
+-- Loads historical price data from file; all data is loaded
+        
 quoteDataFromFile :: FilePath -> IO (V.Vector Quote)
 quoteDataFromFile f = do
   withFile f ReadMode $ \hIn -> do
@@ -53,9 +50,11 @@ quoteDataFromFile f = do
     let qs = CSV.decode HasHeader bs :: Either String (V.Vector Quote)
     case qs of
      Right vs -> return vs 
-     Left e -> return V.empty 
+     Left _ -> return V.empty 
 
-  
+-----------------------------------------------------------------------------------
+-- Download historical price data and save to file; currently only supports Yahoo data
+     
 downloadDataFromServer :: QuoteProvider  -- todo validate filepath ? directoryExists
                        -> Ticker 
                        -> QuoteType 
@@ -71,42 +70,29 @@ downloadDataFromServer qp t@(Ticker s) p nds dir = do
     withHTTP req m $ \resp ->
       withFile (dir ++ (map toUpper s) ++ ".csv") WriteMode $ \hOut -> do
       runEffect $ for (responseBody resp) (liftIO . BS.hPutStr hOut)
-      
-
-s :: Monad m => Pipe Quote [Quote] m ()
-s = PP.scan (\acc v -> v:acc) [] id
-
-quoteFilter :: Monad m => Pipe (Either String Quote) Quote m r
-quoteFilter = go
-  where go = do
-          x <- await
-          case x of
-           Left e -> go
-           Right v -> yield v >> go
 
 mkTimeRange :: UTCTime -> Integer -> TimeRange
 mkTimeRange et numDays = TimeRange st et
   where
     st = UTCTime (addDays (negate $ abs numDays) (utctDay et)) (utctDayTime et)
-    negate i = (-1) * i
 
 
 mkURL :: QuoteProvider -> Ticker -> QuoteType -> TimeRange -> ProviderURL
-mkURL Yahoo (Ticker s) p tr = ProviderURL $ concat ["http://ichart.finance.yahoo.com/table.csv?s="
+mkURL Yahoo (Ticker s) qType tRange = ProviderURL $ concat ["http://ichart.finance.yahoo.com/table.csv?s="
                                                    , map toUpper s
-                                                   , yahooURLParams tr p
+                                                   , yahooURLParams tRange qType
                                                    , "&ignore=.csv"
                                                    ]
   where
     yahooURLParams :: TimeRange -> QuoteType -> String
-    yahooURLParams tr p = schars ++ echars ++ (pchar p)
-      where
-        schars = concat $ zipWith (++) ["&a=","&b=","&c="] $ clist $ toGregorian (utctDay $ startT tr)
-        echars = concat $ zipWith (++) ["&d=","&e=","&f="] $ clist $ toGregorian (utctDay $ endT tr)
-        clist (y,m,d) = [show (m-1), show d, show y]
-        pchar Daily   = "&g=d"
-        pchar Weekly  = "&g=w"
-        pchar Monthly = "&g=m"
+    yahooURLParams tr p = schars tr ++ echars tr ++ (pchar p)
+    schars tr = concat $ zipWith (++) ["&a=","&b=","&c="] $ clist $ toGregorian (utctDay $ startT tr)
+    echars tr = concat $ zipWith (++) ["&d=","&e=","&f="] $ clist $ toGregorian (utctDay $ endT tr)
+    clist (y,m,d) = [show (m-1), show d, show y]
+    pchar Daily   = "&g=d"
+    pchar Weekly  = "&g=w"
+    pchar Monthly = "&g=m"
+mkURL Google _ _ _ = undefined
 
 
 -----------------------------------------------
@@ -144,4 +130,14 @@ withFileData f study= do
     runEffect $ dataProducer >-> study >-> PP.print --}
     where
       quoteProducer hIn = (decode HasHeader (PB.fromHandle hIn) :: Producer (Either String Quote) IO ()) 
+--}
+
+{--
+quoteFilter :: Monad m => Pipe (Either String Quote) Quote m r
+quoteFilter = go
+  where go = do
+          x <- await
+          case x of
+           Left _ -> go
+           Right v -> yield v >> go
 --}
